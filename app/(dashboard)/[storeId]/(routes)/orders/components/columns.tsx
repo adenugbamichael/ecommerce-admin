f243e508b4895dff3f13a90664c0b840148eeb3b
adenugbamichael <adenugbamicke@gmail.com>
2023-09-09 09:28:43 +0100

    Wrap up order page/form

--- app/(dashboard)/[storeId]/(routes)/orders/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/components/columns.tsx
@@ -2,27 +2,35 @@
 
 import { ColumnDef } from "@tanstack/react-table"
 
-import { CellAction } from "./cell-action"
-
-// This type is used to define the shape of our data.
-// You can use a Zod schema here if you want.
-export type BillboardColumn = {
+export type OrderColumn = {
   id: string
-  label: string
+  phone: string
+  address: string
+  isPaid: boolean
+  totalPrice: string
+  products: string
   createdAt: string
 }
 
-export const columns: ColumnDef<BillboardColumn>[] = [
+export const columns: ColumnDef<OrderColumn>[] = [
   {
-    accessorKey: "label",
-    header: "Label",
+    accessorKey: "products",
+    header: "Products",
   },
   {
-    accessorKey: "createdAt",
-    header: "Date",
+    accessorKey: "phone",
+    header: "Phone",
   },
   {
-    id: "actions",
-    cell: ({ row }) => <CellAction data={row.original} />,
+    accessorKey: "address",
+    header: "Address",
+  },
+  {
+    accessorKey: "totalPrice",
+    header: "Total price",
+  },
+  {
+    accessorKey: "isPaid",
+    header: "Paid",
   },
 ]
